b1c193ca1173d041972e9533c70c7c98e99c43a3
Hadrien lamotte <hadrien.lamotte@gmail.com>
2017-04-21 22:42:54 +0200

    Update code to use POO

--- Generation/contact.py
+++ Generation/contact.py
@@ -1,5 +1,7 @@
 import os
 import faker
+import json
+from collections import OrderedDict
 
 story_type = 'Contact'
 
@@ -9,58 +11,101 @@ timeout_subject = 'Customer went to concurrency'
 
 body_init = ''
 
-def create_object(start_id, current_id, story_name, sender, subject,
-        body, keywords, score, is_bad, is_timeout):
-    res = "{" + os.linesep
-    res += '  "story_name": "' + story_name + '",' + os.linesep
-    res += '  "idLink": ' + str(current_id) + ',' + os.linesep
-    res += '  "sender": "' + sender + '",' + os.linesep
-    res += '  "subject": "' + subject + '",' + os.linesep
-    res += '  "body": "' + body + '",' + os.linesep
-    res += '  "good": {' + os.linesep
-    if is_timeout:
-        res += '    "score": ' + str(-score) + ',' + os.linesep
-        res += '    "link": null,' + os.linesep
-    else:
-        res += '    "keywords": [' + os.linesep
-        for i in keywords:
-            res += '      "' + i + '"'
-            if i != keywords[len(keywords) - 1]:
-                res += ',' + os.linesep
-        res += os.linesep + '    ],' + os.linesep
-        res += '    "score": ' + str(score) + ',' + os.linesep
-        res += '    "link": ' + str(start_id + 3) + ',' + os.linesep
-    res += '  },' + os.linesep
-    res += '  "bad": {' + os.linesep
-    res += '    "score": ' + str(-score) + ',' + os.linesep
-    if is_bad:
-        res += '    "link": ' + str(start_id + 2) + ',' + os.linesep
-    elif is_timeout:
-        res += '    "link": null,' + os.linesep
-    else:
-        res += '    "link": ' + str(start_id + 1) + ',' + os.linesep
-    res += '  },' + os.linesep
-    res += '  "timeOut": {' + os.linesep
-    res += '    "score": ' + str(-score) + ',' + os.linesep
-    if not is_timeout:
-        res += '    "link": ' + str(start_id + 2) + ',' + os.linesep
-    else:
-        res += '    "link": null,' + os.linesep
-    res += '}'
-    return res
+class Linker():
+    linkId = 0
+    score = 0
+    def __init__(self, score, linkId, is_bad, is_timeout):
+        self.score = score
+        self.linkId = linkId
+
+class Good(Linker):
+    def __init__(self, score, linkId, is_bad, is_timeout):
+        Linker.__init__(self, score, linkId, is_bad, is_timeout)
+        self.linkId = linkId + 3
+        
+class Bad(Linker):
+    def __init__(self, score, linkId, is_bad, is_timeout):
+        Linker.__init__(self, score, linkId, is_bad, is_timeout)
+        if is_bad:
+            self.linkId = linkId + 2
+        elif is_timeout:
+            self.linkId = None
+        else:
+            self.linkId = linkId + 1
+        self.score = -score;
+
+class TimeOut(Linker):
+    def __init__(self, score, linkId, is_bad, is_timeout):
+        Linker.__init__(self, score, linkId, is_bad, is_timeout)
+        if is_timeout:
+            self.linkId = None
+        else:
+            self.linkId = linkId + 2
+        self.score = -score;
+        
+
+class quest():
+    start_id = 0
+    story_name = ""
+    sender = ""
+    subject = ""
+    body = ""
+    keywords = []
+    score = 0
+    is_bad = False
+    is_timeout = False
+    good = None
+    bad = None
+    timeOut = None
+    serialize = OrderedDict()
+    
+    def __init__(self, start_id, current_id, story_name, sender, subject, body, keywords, score, is_bad, is_timeout):
+        self.start_id = current_id
+        self.story_name = story_name
+        self.sender = sender
+        self.subject = subject
+        self.body = body
+        self.keywords = keywords
+        self.score = score
+        self.is_bad = is_bad
+        self.is_timeout = is_timeout
+        if is_timeout:
+            self.good = TimeOut(score, start_id, is_bad, is_timeout)
+        else:
+            self.good = Good(score, start_id, is_bad, is_timeout)
+        self.bad = Bad(score, start_id, is_bad, is_timeout)
+        self.timeOut = TimeOut(score, start_id, is_bad, is_timeout)
+
+    def toJSON(self):
+        self.serialize['start_id'] = self.start_id
+        self.serialize['story_name'] = self.story_name
+        self.serialize['sender'] = self.sender
+        self.serialize['subject'] = self.subject
+        self.serialize['body'] = self.body
+        self.serialize['keywords'] = self.keywords
+        self.serialize['score'] = self.score
+        self.serialize['good'] = self.good
+        self.serialize['bad'] = self.bad
+        self.serialize['timeOut'] = self.timeOut
+        
+        return json.dumps(self.serialize, default=lambda o: o.__dict__, 
+            sort_keys=False, indent=4)
 
 def generate(start_id, sender, score, is_last, story_name):
-    json = ''
-    json += create_object(start_id, start_id, story_name, sender, init_subject,
+    questList = []
+    init = quest(start_id, start_id, story_name, sender, init_subject,
             "init" + story_type  + "Quest" + str(start_id) + ".md", ['izi', 'php'], score, False, False)
-    json += ',' + os.linesep
-    json += create_object(start_id, start_id + 1, story_name, sender, bad_subject,
-            "bad" + story_type + "Quest" + str(start_id) + ".md", ['izi', 'php'], score, True, False)
-    json += ',' + os.linesep
-    json += create_object(start_id, start_id + 2, story_name, sender, timeout_subject,
+    bad = quest(start_id, start_id + 1, story_name, sender, bad_subject,
+            "bad" + story_type  + "Quest" + str(start_id) + ".md", ['izi', 'php'], score, True, False)
+    timeOut = quest(start_id, start_id + 2, story_name, sender, timeout_subject,
             "timeOut" + story_type  + "Quest" + str(start_id) + ".md", ['izi', 'php'], score, False, True)
-    print(json)
-    return json
+    questList.append(init)
+    questList.append(bad)
+    questList.append(timeOut)
+
+    for x in questList:
+        print(x.toJSON())
+    return questList
 
 def generateInitQuestMail():
     res = ''
@@ -68,6 +113,5 @@ def generateInitQuestMail():
     res += body_init + os.linesep
     res += 'Formules de courtoisie' + os.linesep
     res += 'Signature' + os.linesep
-    print("lol")
 
 generate(1, "johndoe@gmail.com", 400, False, "plop")
